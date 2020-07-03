72c185612b2a11502dceba01ab388b16518f4d84
Paremir <67802472+Paremir@users.noreply.github.com>
2020-07-03 23:00:15 +0300

    Update Hello_World.spec.js

--- Hello_World.spec.js
+++ Hello_World.spec.js
@@ -3,19 +3,18 @@ context('Hello World', () => {
   it('Go to the site',() =>{
     cy.visit('https://pastebin.com/')
   })
-
-  it('We write "Hello World" and creat link', () =>{
+  it('We write "Hello World" and create link', () =>{
     cy.log('write "Hello World" in text box')
     cy.get('.paste_textarea')
       .type('Hello World')
     cy.log('Create link')
     cy.get('.btnbig').click()
   })
-  it('Check our record', () =>{
+  it('Check new paste', () =>{
     cy.log('Check link')
     cy.url()
       .should('include','https://pastebin.com')
-    cy.log('Check "Hello world in RAW Paste Data"')
+    cy.log('Check "Hello World" in RAW Paste Data')
     cy.get('.paste_code')
       .should('have.value','Hello World')
   })
